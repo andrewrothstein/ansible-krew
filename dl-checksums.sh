d05ac139e176d3902e4912302b6e4edfdf768207
Andrew Rothstein <andrew.rothstein@gmail.com>
2021-11-02 00:06:50 -0400

    krew:0.4.2

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -2,14 +2,28 @@
 set -e
 readonly MIRROR=https://github.com/kubernetes-sigs/krew/releases/download
 
-dl_ver() {
+dlver() {
     local ver=$1
-    local archive_type=${2:-tar.gz}
+    local os=$2
+    local arch=$3
+    local archive_type=${4:-tar.gz}
+    local platform="${os}"_${arch}
+    # https://github.com/kubernetes-sigs/krew/releases/download/v0.4.2/krew-linux_amd64.tar.gz.sha256
+    local url="${MIRROR}/v${ver}/krew-${platform}.${archive_type}.sha256"
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(curl -sSL $url)
 
-    # https://github.com/kubernetes-sigs/krew/releases/download/v0.4.1/krew.tar.gz.sha256
-    local url="${MIRROR}/v${ver}/krew.${archive_type}.sha256"
-    printf "  # %s\n" $url
-    printf "  '%s': sha256:%s\n" $ver $(curl -sSL $url | awk '{print $1}')
 }
 
-dl_ver ${1:-0.4.1}
+dl() {
+    local ver=$1
+    printf "  '%s':\n" $ver
+    dlver $ver darwin amd64
+    dlver $ver darwin arm64
+    dlver $ver linux amd64
+    dlver $ver linux arm
+    dlver $ver linux arm64
+    dlver $ver windows amd64
+}
+
+dl ${1:-0.4.2}
